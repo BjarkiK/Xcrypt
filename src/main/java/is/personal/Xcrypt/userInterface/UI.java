b34a240873343ff918d49f9a839da16f18e21fba
Bjarki K <bjarkik16@ru.is>
2017-12-29 03:58:22 +0000

    Added simple design and few mouse events on button

--- src/main/java/is/personal/Xcrypt/userInterface/UI.java
+++ src/main/java/is/personal/Xcrypt/userInterface/UI.java
@@ -1,7 +1,7 @@
 package main.java.is.personal.Xcrypt.userInterface;
 
 import java.awt.EventQueue;
-import java.awt.Image;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JTextField;
@@ -10,6 +10,7 @@ import javax.swing.JRadioButton;
 import main.java.is.personal.Xcrypt.connection.Run;
 
 import javax.swing.SpringLayout;
+import javax.swing.AbstractButton;
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -18,9 +19,11 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.font.TextAttribute;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Map;
 
 import javax.swing.JLabel;
 import java.awt.Color;
@@ -30,6 +33,7 @@ import javax.swing.JPasswordField;
 import javax.swing.JProgressBar;
 import javax.swing.JCheckBox;
 import javax.swing.JFileChooser;
+
 
 public class UI {
 
@@ -43,6 +47,8 @@ public class UI {
 	private JTextField textFieldUsername;
 	private JRadioButton rdbtnEncryp;
 	private JRadioButton rdbtnDecrypt;
+	private static JButton btnStart;
+	private JLabel BGImage;
 
 	/**
 	 * Launch the application.
@@ -76,7 +82,8 @@ public class UI {
 	private void initialize() throws IOException {
 		
 		// Render main window
-		frmEncrypt = new JFrame();
+		frmEncrypt = new JFrame();;
+		frmEncrypt.setFont(new Font("Cambria", Font.PLAIN, 14));
 		frmEncrypt.setResizable(false);
 		frmEncrypt.setIconImage(Toolkit.getDefaultToolkit().getImage("img\\design\\Icon.png"));
 		frmEncrypt.setTitle("Xcrypt");
@@ -105,6 +112,8 @@ public class UI {
 		
 		
 		JCheckBox chckbxEncryptOnExit = new JCheckBox("Encrypt on exit");
+		chckbxEncryptOnExit.setFont(new Font("Cambria", Font.PLAIN, 14));
+		disableBackground(chckbxEncryptOnExit);
 		chckbxEncryptOnExit.setSelected(true);
 		springLayout.putConstraint(SpringLayout.NORTH, chckbxEncryptOnExit, 215, SpringLayout.NORTH, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.WEST, chckbxEncryptOnExit, 22, SpringLayout.WEST, frmEncrypt.getContentPane());
@@ -121,11 +130,15 @@ public class UI {
 		
 		
 		JLabel pathLable = new JLabel("Enter folder path");
+		pathLable.setFont(new Font("Cambria", Font.BOLD, 14));
 		springLayout.putConstraint(SpringLayout.NORTH, pathLable, 15, SpringLayout.NORTH, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.WEST, pathLable, 25, SpringLayout.WEST, frmEncrypt.getContentPane());
 		frmEncrypt.getContentPane().add(pathLable);
 		
 		textFieldPath = new JTextField();
+		textFieldPath.setFont(new Font("Cambria", Font.PLAIN, 14));
+		textFieldPath.setOpaque(false);
+		textFieldPath.setBorder(javax.swing.BorderFactory.createEmptyBorder());
 		springLayout.putConstraint(SpringLayout.NORTH, textFieldPath, 35, SpringLayout.NORTH, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, textFieldPath, 60, SpringLayout.NORTH, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.WEST, textFieldPath, 25, SpringLayout.WEST, frmEncrypt.getContentPane());
@@ -134,20 +147,25 @@ public class UI {
 		
 		
 		JLabel pathErrorLable = new JLabel("");
+		pathErrorLable.setFont(new Font("Cambria", Font.PLAIN, 14));
 		springLayout.putConstraint(SpringLayout.NORTH, pathErrorLable, 60, SpringLayout.NORTH, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.WEST, pathErrorLable, 25, SpringLayout.WEST, frmEncrypt.getContentPane());
-		pathErrorLable.setForeground(Color.RED);
+		pathErrorLable.setForeground(Color.ORANGE);
 		frmEncrypt.getContentPane().add(pathErrorLable);
 		pathErrorLable.hide();
 		
 		
 		
 		JLabel usernameLable = new JLabel("Enter username");
+		usernameLable.setFont(new Font("Cambria", Font.BOLD, 14));
 		springLayout.putConstraint(SpringLayout.NORTH, usernameLable, 80, SpringLayout.NORTH, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.WEST, usernameLable, 25, SpringLayout.WEST, frmEncrypt.getContentPane());
 		frmEncrypt.getContentPane().add(usernameLable);
 		
 		textFieldUsername = new JTextField();
+		textFieldUsername.setFont(new Font("Cambria", Font.PLAIN, 14));
+		textFieldUsername.setOpaque(false);
+		textFieldUsername.setBorder(javax.swing.BorderFactory.createEmptyBorder());
 		springLayout.putConstraint(SpringLayout.NORTH, textFieldUsername, 100, SpringLayout.NORTH, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, textFieldUsername, 125, SpringLayout.NORTH, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.WEST, textFieldUsername, 25, SpringLayout.WEST, frmEncrypt.getContentPane());
@@ -156,7 +174,8 @@ public class UI {
 		frmEncrypt.getContentPane().add(textFieldUsername);
 		
 		JLabel usernameErrorLable = new JLabel("Username can't be empty");
-		usernameErrorLable.setForeground(Color.RED);
+		usernameErrorLable.setFont(new Font("Cambria", Font.PLAIN, 14));
+		usernameErrorLable.setForeground(Color.ORANGE);
 		springLayout.putConstraint(SpringLayout.NORTH, usernameErrorLable, 125, SpringLayout.NORTH, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.WEST, usernameErrorLable, 25, SpringLayout.WEST, frmEncrypt.getContentPane());
 		frmEncrypt.getContentPane().add(usernameErrorLable);
@@ -166,21 +185,25 @@ public class UI {
 		
 		
 		JLabel passwordLable = new JLabel("Enter password");
+		passwordLable.setFont(new Font("Cambria", Font.BOLD, 14));
 		springLayout.putConstraint(SpringLayout.NORTH, passwordLable, 145, SpringLayout.NORTH, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.WEST, passwordLable, 25, SpringLayout.WEST, frmEncrypt.getContentPane());
 		frmEncrypt.getContentPane().add(passwordLable);
 		
 		fieldPassword = new JPasswordField();
+		fieldPassword.setOpaque(false);
+		fieldPassword.setBorder(javax.swing.BorderFactory.createEmptyBorder());
 		springLayout.putConstraint(SpringLayout.NORTH, fieldPassword, 165, SpringLayout.NORTH, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, fieldPassword, 190, SpringLayout.NORTH, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.WEST, fieldPassword, 25, SpringLayout.WEST, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, fieldPassword, 250, SpringLayout.WEST, frmEncrypt.getContentPane());
 		frmEncrypt.getContentPane().add(fieldPassword);
 
-		JLabel passwordErrorLable = new JLabel("Password cant be empty");
+		JLabel passwordErrorLable = new JLabel("Password can't be empty");
+		passwordErrorLable.setFont(new Font("Cambria", Font.PLAIN, 14));
 		springLayout.putConstraint(SpringLayout.NORTH, passwordErrorLable, 190, SpringLayout.NORTH, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.WEST, passwordErrorLable, 25, SpringLayout.WEST, frmEncrypt.getContentPane());
-		passwordErrorLable.setForeground(Color.RED);
+		passwordErrorLable.setForeground(Color.ORANGE);
 		frmEncrypt.getContentPane().add(passwordErrorLable);
 		passwordErrorLable.hide();
 
@@ -189,6 +212,7 @@ public class UI {
 
 		
 		JProgressBar progressBar = new JProgressBar();
+		progressBar.setFont(new Font("Cambria", Font.PLAIN, 12));
 		springLayout.putConstraint(SpringLayout.WEST, progressBar, 35, SpringLayout.WEST, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, progressBar, -10, SpringLayout.SOUTH, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, progressBar, -28, SpringLayout.EAST, frmEncrypt.getContentPane());
@@ -197,14 +221,21 @@ public class UI {
 		frmEncrypt.getContentPane().add(progressBar);
 		
 		
-		JButton btnStart = new JButton("Start");
+		
+		btnStart = new JButton("Start");
+		btnStart.setFont(new Font("Cambria", Font.PLAIN, 59));
+		disableBackground(btnStart);
 		springLayout.putConstraint(SpringLayout.NORTH, btnStart, 260, SpringLayout.NORTH, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, btnStart, -50, SpringLayout.SOUTH, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.WEST, btnStart, 50, SpringLayout.WEST, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, btnStart, -50, SpringLayout.EAST, frmEncrypt.getContentPane());
 		btnStart.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseReleased(MouseEvent arg0) {
+			public void mouseReleased(MouseEvent e) {
+				setMainBackgroundImage("bgImageExtended.png");
+				if(mouseReleasedInsideButton(e) == false){
+					return;
+				}
 				String path = textFieldPath.getText();
 				String username = textFieldUsername.getText();
 				String password = fieldPassword.getText();
@@ -261,10 +292,23 @@ public class UI {
 					}
 					try {
 						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
+					} catch (InterruptedException err) {
+						err.printStackTrace();
 					}
 				}
+			}
+			@Override
+			public void mouseEntered(MouseEvent arg0) {		
+				btnStart.setForeground(new Color(63, 132, 216));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnStart.setForeground(Color.BLACK);
+				setMainBackgroundImage("bgImageExtended.png");
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				setMainBackgroundImage("bgImageExtendedBtnClicked.png");
 			}
 		});
 		frmEncrypt.getContentPane().add(btnStart);	
@@ -273,9 +317,8 @@ public class UI {
 		fieldPassword.setText("bjarki");
 		textFieldPath.setText("D:\\photos - Copy");
 		
-		JLabel BGImage = new JLabel("");
-		ImageIcon backgroundImage = new ImageIcon("img\\design\\bgImage.png");//this.getClass().getResource("/bgImage.png")).getImage();
-		BGImage.setIcon(backgroundImage);
+		BGImage = new JLabel("");
+		setMainBackgroundImage("bgImageExtended.png");
 		springLayout.putConstraint(SpringLayout.NORTH, BGImage, 0, SpringLayout.NORTH, frmEncrypt.getContentPane());
 		springLayout.putConstraint(SpringLayout.WEST, BGImage, 0, SpringLayout.WEST, frmEncrypt.getContentPane());
 		frmEncrypt.getContentPane().add(BGImage);
@@ -337,8 +380,8 @@ public class UI {
 	
 	private JRadioButton rdbtnEncrypt(SpringLayout springLayout, JCheckBox chckbxEncryptOnExit){
 		rdbtnEncryp = new JRadioButton("Encrypt");
-//		ImageIcon backgroundImage = new ImageIcon("img\\design\\RadioBtn.png");//this.getClass().getResource("/bgImage.png")).getImage();
-//		rdbtnEncryp.setBackground(); .setIcon(backgroundImage);
+		rdbtnEncryp.setFont(new Font("Cambria", Font.PLAIN, 14));
+		disableBackground(rdbtnEncryp);
 		rdbtnEncryp.addMouseListener(new MouseAdapter() {
 			@SuppressWarnings("deprecation")
 			@Override
@@ -355,6 +398,8 @@ public class UI {
 	
 	private JRadioButton rdbtnDecrypt(SpringLayout springLayout, JCheckBox chckbxEncryptOnExit){
 		rdbtnDecrypt = new JRadioButton("Decrypt");
+		rdbtnDecrypt.setFont(new Font("Cambria", Font.PLAIN, 14));
+		disableBackground(rdbtnDecrypt);
 		rdbtnDecrypt.addMouseListener(new MouseAdapter() {
 			@SuppressWarnings("deprecation")
 			@Override
@@ -366,6 +411,28 @@ public class UI {
 		springLayout.putConstraint(SpringLayout.WEST, rdbtnDecrypt, 330, SpringLayout.WEST, frmEncrypt.getContentPane());
 		frmEncrypt.getContentPane().add(rdbtnDecrypt);
 		return rdbtnDecrypt;
+	}
+	
+	private boolean mouseReleasedInsideButton(MouseEvent e){
+		double btnHeight = btnStart.getSize().getHeight();
+		double btnWidth = btnStart.getSize().getWidth();
+		
+		if (e.getX() >= 0 && e.getX() <= btnWidth && e.getY() >= 0 && e.getY() <= btnHeight ){
+			return true;
+		}
+		return false;
+	}
+	
+	private void disableBackground(AbstractButton element){
+		element.setOpaque(false);
+		element.setContentAreaFilled(false);
+		element.setBorderPainted(false);
+		element.setFocusPainted(false);
+	}
+	
+	private void setMainBackgroundImage(String imageName){
+		ImageIcon backgroundImage = new ImageIcon("img\\design\\" + imageName);
+		BGImage.setIcon(backgroundImage);
 	}
 	
 	private void grouprdbtn(JRadioButton rdbtnEncrypt, JRadioButton rdbtnDecrypt){
